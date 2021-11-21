30c7d6838975cf2fba4c25d938fdd0114d1a9163
nahom-neo <nahom.mehanzel@neofinancial.com>
2021-11-20 20:43:28 -0700

    add about route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || "3000";
 
+app.get('/about', (req,res)=>{
+    res.send("About Page!")
+})
+
 app.get("/", (req, res) => {
   res.send("Welcome to Express");
 });
